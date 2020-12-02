5f386f8e671e90b4b9a0346fb2637bf776b1fb57
dhaniyapatta <ajha211@gmail.com>
2020-12-02 20:12:22 +0530

    updates

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/ankit/catkin_ws/src
+# Build directory: /home/ankit/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlenpoly")
+subdirs("beginner_tutorials")
+subdirs("rugved_description")
+subdirs("robot_setup_tf")
+subdirs("learning_tf")
+subdirs("m2wr_description")
---
+++ build/turtlenpoly2/CMakeFiles/turtlenpoly.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtlenpoly.dir/turtlenpoly.cpp.o"
+  "/home/ankit/catkin_ws/devel/lib/turtlenpoly/turtlenpoly.pdb"
+  "/home/ankit/catkin_ws/devel/lib/turtlenpoly/turtlenpoly"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtlenpoly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

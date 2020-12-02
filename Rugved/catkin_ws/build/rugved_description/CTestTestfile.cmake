15d643065e3534316c5303d7fc2f727e219e0993
dhaniyapatta <ajha211@gmail.com>
2020-12-02 20:21:25 +0530

    rug

---
+++ Rugved/catkin_ws/build/rugved_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ankit/catkin_ws/src/rugved_description
+# Build directory: /home/ankit/catkin_ws/build/rugved_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Rugved/catkin_ws/build/turtlenpoly2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ankit/catkin_ws/src/turtlenpoly2
+# Build directory: /home/ankit/catkin_ws/build/turtlenpoly2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
